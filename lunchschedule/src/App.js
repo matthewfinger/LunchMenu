d64c8b41e3232c868ce0fd394232481c7b0aaff6
Matt Finger <matthewfinger3@gmail.com>
2021-03-02 12:24:46 -0600

    created basic framework

---
+++ lunchschedule/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="locationsbox">Locations will be listed here</div>
+      <div id="info">Location Lunch Information Will be listed here</div>
+    </div>
+  );
+}
+
+export default App;
